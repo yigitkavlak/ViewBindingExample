555692e9381ac669e697a32b9feeafa3a64f6c2a
yigitkavlak <yigitkavlak97@gmail.com>
2021-02-09 02:42:17 +0300

    view binding implementation

--- app/src/main/java/com/yigitkavlak/viewbindingexample/MainActivity.kt
+++ app/src/main/java/com/yigitkavlak/viewbindingexample/MainActivity.kt
@@ -2,12 +2,26 @@ package com.yigitkavlak.viewbindingexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.yigitkavlak.viewbindingexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var namer : String
+      binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+
+    }
+
+    fun changeText(view: View) {
+
+
+        if(binding.textView.text.equals("TextView is Changed.")){
+            binding.textView.text = "View Binding Example"
+        }else binding.textView.text = "TextView is Changed."
     }
 }
